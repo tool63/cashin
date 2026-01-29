2e7c1f32c1d080fadf9929eb6ed4d7f54a858c75
tool63 <toolteller@gmail.com>
2026-01-30 00:52:45 +0600

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Cashin is Live 🚀</h1>
+      <p>Your earning platform has started.</p>
+    </main>
+  );
+}
