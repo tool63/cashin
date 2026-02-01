47eb283556d528036f34f29c8dc8ed394da1a7f4
tool63 <toolteller@gmail.com>
2026-02-02 00:10:54 +0600

    Update page.tsx

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,9 +1,10 @@
 "use client"
 
-import { useParams } from 'next/navigation'
+import { useParams } from "next/navigation"
 
 export default function DynamicPage() {
   const params = useParams()
+
   return (
     <main className="p-6 text-center">
       <h1 className="text-2xl font-bold">Dynamic Page</h1>
