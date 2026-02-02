5bba06a8513a6f453327a69dcae01ab92923c69c
tool63 <toolteller@gmail.com>
2026-02-02 23:40:37 +0600

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,16 +2,12 @@
 
 import { ThemeProvider } from "next-themes"
 
-export default function Providers({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider
       attribute="class"
-      defaultTheme="dark"
-      enableSystem={false}
+      defaultTheme="system"
+      enableSystem
     >
       {children}
     </ThemeProvider>
