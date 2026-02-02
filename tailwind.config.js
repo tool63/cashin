6c5e945e1dce99b875f827159ee445186c96c1f2
tool63 <toolteller@gmail.com>
2026-02-02 21:12:03 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class", // <-- enable dark/light toggle
   content: [
     "./app/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}"
   ],
+  darkMode: "class", // enable dark mode using a class
   theme: {
     extend: {},
   },
