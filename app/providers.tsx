e0692806cdcf08e97517fa384ae03daaa53eda82
tool63 <toolteller@gmail.com>
2026-02-02 21:13:08 +0600

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,15 +1,17 @@
-"use client"
+"use client";
 
-import { ThemeProvider } from "next-themes"
+import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider
       attribute="class"
-      defaultTheme="dark"
-      enableSystem={false}
+      defaultTheme="dark" // default dark
+      enableSystem={false} // users cannot auto-follow system
     >
       {children}
     </ThemeProvider>
-  )
+  );
 }
+
+export default Providers;
