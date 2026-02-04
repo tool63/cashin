f3eca570f28049383dfe0a933916c0772f24107e
tool63 <toolteller@gmail.com>
2026-02-04 22:47:24 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack(config) {
+    // Add alias for '@' to point to the project root
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": require("path").resolve(__dirname),
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
