dd47563a5687b5fcb6c7488ebccb01a1b50802e9
tool63 <toolteller@gmail.com>
2026-02-04 23:47:35 +0600

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,6 @@
-/** @type {import('postcss').ProcessOptions} */
 module.exports = {
   plugins: {
-    // Tailwind CSS framework
     tailwindcss: {},
-    
-    // Automatically add vendor prefixes for cross-browser compatibility
     autoprefixer: {},
   },
-}
+};
