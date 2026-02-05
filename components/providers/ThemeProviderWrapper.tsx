37cfeeafed420bf1630627e0a96f4c5057e29eb2
tool63 <toolteller@gmail.com>
2026-02-05 17:07:04 +0600

    Create ThemeProviderWrapper.tsx

---
+++ components/providers/ThemeProviderWrapper.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { ThemeProvider } from "next-themes"
+import { useEffect, useState } from "react"
+
+export default function ThemeProviderWrapper({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => setMounted(true), [])
+
+  if (!mounted) return null
+
+  return (
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+      {children}
+    </ThemeProvider>
+  )
+}
