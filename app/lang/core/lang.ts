2a0ef2428b376fdde7867d6104d07c9db12a284a
tool63 <toolteller@gmail.com>
2026-02-06 18:37:38 +0600

    Create lang.ts

---
+++ app/lang/core/lang.ts
@@ -0,0 +1,27 @@
+// app/lang/core/lang.ts
+export const coreLang = {
+  header: {
+    howItWorks: "How It Works",
+    earn: "Earn",
+    earnOptions: {
+      surveys: "Surveys",
+      appInstalls: "App Installs",
+      playGames: "Playing Games",
+      watchVideos: "Watching Videos",
+      offerwall: "Offerwall",
+    },
+    cashout: "Cashout",
+    blog: "Blog",
+    help: "Help",
+    login: "Login",
+    signUp: "Sign Up",
+    lightMode: "Light Mode",
+    darkMode: "Dark Mode",
+  },
+  footer: {
+    about: "About",
+    contact: "Contact",
+    privacy: "Privacy Policy",
+    terms: "Terms of Service",
+  },
+}
