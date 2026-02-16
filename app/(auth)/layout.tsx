254c9cb7357163a81d3e51be0cf9eb3609e2d697
tool63 <toolteller@gmail.com>
2026-02-17 02:26:59 +0600

    Update layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -8,10 +8,10 @@ interface AuthLayoutProps {
 
 export default function AuthLayout({ children }: AuthLayoutProps) {
   return (
-    <div className="flex items-center justify-center w-screen min-h-screen px-4 sm:px-6 py-12">
+    <div className="flex items-center justify-center w-screen min-h-screen px-4 sm:px-6 py-12 bg-gray-50 dark:bg-[#070A14]">
       
-      {/* Front Layer: Gradient card */}
-      <div className="w-full max-w-xl bg-gradient-to-r from-yellow-400 via-green-400 to-green-500 p-1 rounded-3xl shadow-2xl">
+      {/* Front Layer: Gradient Card */}
+      <div className="w-full max-w-md sm:max-w-lg bg-gradient-to-r from-yellow-400 via-green-400 to-green-500 p-1 rounded-3xl shadow-2xl">
         <div className="bg-white dark:bg-[#070A14] rounded-2xl p-6 sm:p-8 flex flex-col gap-6">
           {children}
         </div>
