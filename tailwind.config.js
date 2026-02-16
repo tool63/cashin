3d2bcd637f1fcedb48d4621b9bd9591dfa789855
tool63 <toolteller@gmail.com>
2026-02-16 23:37:44 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,26 @@
 module.exports = {
   content: [
     "./app/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}"
+    "./components/**/*.{ts,tsx}",
   ],
   darkMode: "class", // enable dark mode using a class
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        gradient: "gradient 8s ease infinite",
+        float: "float 6s ease-in-out infinite",
+      },
+      keyframes: {
+        gradient: {
+          "0%, 100%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+        },
+        float: {
+          "0%, 100%": { transform: "translateY(0px)" },
+          "50%": { transform: "translateY(-20px)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
