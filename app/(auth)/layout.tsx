12fe3c10125332dff399da8323dd8388a56b1154
tool63 <toolteller@gmail.com>
2026-02-17 12:00:38 +0600

    Update layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -18,10 +18,17 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
         {/* ============================
             Floating Blobs Around Card
         ============================ */}
-        <div className="absolute -top-16 -left-16 w-32 h-32 bg-yellow-400/30 rounded-full blur-3xl animate-float pointer-events-none z-0"></div>
-        <div className="absolute -top-16 -right-16 w-32 h-32 bg-green-400/30 rounded-full blur-3xl animate-float animation-delay-1000 pointer-events-none z-0"></div>
-        <div className="absolute -bottom-16 -left-16 w-32 h-32 bg-yellow-400/30 rounded-full blur-3xl animate-float animation-delay-2000 pointer-events-none z-0"></div>
-        <div className="absolute -bottom-16 -right-16 w-32 h-32 bg-green-400/30 rounded-full blur-3xl animate-float animation-delay-3000 pointer-events-none z-0"></div>
+        {/* Top Left */}
+        <div className="absolute -top-20 -left-20 w-32 h-32 bg-yellow-400/30 rounded-full blur-3xl animate-float pointer-events-none z-0"></div>
+
+        {/* Top Right */}
+        <div className="absolute -top-20 right-[-80px] w-32 h-32 bg-green-400/30 rounded-full blur-3xl animate-float animation-delay-1000 pointer-events-none z-0"></div>
+
+        {/* Bottom Left */}
+        <div className="absolute bottom-[-80px] -left-20 w-32 h-32 bg-yellow-400/30 rounded-full blur-3xl animate-float animation-delay-2000 pointer-events-none z-0"></div>
+
+        {/* Bottom Right */}
+        <div className="absolute bottom-[-80px] right-[-80px] w-32 h-32 bg-green-400/30 rounded-full blur-3xl animate-float animation-delay-3000 pointer-events-none z-0"></div>
 
         {/* ============================
             Scrollable Content Card
