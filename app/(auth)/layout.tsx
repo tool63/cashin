8da9fa757c44628d9988e486b39eae8a4985b9e5
tool63 <toolteller@gmail.com>
2026-02-17 21:21:30 +0600

    Update layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -10,7 +10,7 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
   return (
     <div
       className="
-        w-screen min-h-screen bg-[#0B0F1A]
+        w-screen h-screen bg-[#0B0F1A]
         flex justify-center items-center px-4 sm:px-6
         overflow-hidden
       "
