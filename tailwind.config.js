93b5f62760b831abf62a167d9130c91422041c5f
tool63 <toolteller@gmail.com>
2026-02-19 19:39:25 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,13 +13,21 @@ module.exports = {
       },
       keyframes: {
         gradient: {
-          "0%, 100%": { backgroundPosition: "0% 50%" },
-          "50%": { backgroundPosition: "100% 50%" },
+          "0%, 100%": { backgroundPosition: "0% 50%", backgroundSize: "200% 200%" },
+          "50%": { backgroundPosition: "100% 50%", backgroundSize: "200% 200%" },
         },
         float: {
           "0%, 100%": { transform: "translate3d(0, 0, 0)" },
-          "50%": { transform: "translate3d(0, -20px, 0)" }, // Y only
+          "25%": { transform: "translate3d(-10px, -10px, 0)" },
+          "50%": { transform: "translate3d(0, -20px, 0)" },
+          "75%": { transform: "translate3d(10px, -10px, 0)" },
         },
+      },
+      // optional custom animation delays
+      animationDelay: {
+        1000: "1s",
+        2000: "2s",
+        3000: "3s",
       },
     },
   },
