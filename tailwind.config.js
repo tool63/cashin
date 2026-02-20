ad55757a605097619f985c15eef1aa73a514e9e3
tool63 <toolteller@gmail.com>
2026-02-20 14:32:55 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ module.exports = {
       animation: {
         gradient: "gradient 15s ease infinite",
         float: "float 8s ease-in-out infinite",
+        blobMove: "blobMove 30s linear infinite",
+        blobMove2: "blobMove2 45s linear infinite",
       },
       keyframes: {
         gradient: {
@@ -18,7 +20,21 @@ module.exports = {
         },
         float: {
           "0%, 100%": { transform: "translate3d(0, 0, 0)" },
-          "50%": { transform: "translate3d(0, -20px, 0)" }, // Y only
+          "50%": { transform: "translate3d(0, -20px, 0)" },
+        },
+        blobMove: {
+          "0%": { transform: "translate(0, 0) rotate(0deg)" },
+          "25%": { transform: "translate(20px, -30px) rotate(45deg)" },
+          "50%": { transform: "translate(-20px, 20px) rotate(90deg)" },
+          "75%": { transform: "translate(30px, 10px) rotate(135deg)" },
+          "100%": { transform: "translate(0, 0) rotate(360deg)" },
+        },
+        blobMove2: {
+          "0%": { transform: "translate(0, 0) rotate(0deg)" },
+          "25%": { transform: "translate(-25px, 15px) rotate(-45deg)" },
+          "50%": { transform: "translate(20px, -20px) rotate(-90deg)" },
+          "75%": { transform: "translate(-15px, 25px) rotate(-135deg)" },
+          "100%": { transform: "translate(0, 0) rotate(-360deg)" },
         },
       },
     },
