9acbea58e736e2d69dee0eaf4ff5880561e0546d
tool63 <toolteller@gmail.com>
2026-02-21 15:28:18 +0600

    Update AuthModal.tsx

--- components/modals/AuthModal.tsx
+++ components/modals/AuthModal.tsx
@@ -19,8 +19,13 @@ export default function AuthModal({ children }: AuthModalProps) {
     <div
       className="
         relative w-full max-w-md
-        bg-gradient-to-br from-yellow-400/20 via-green-400/30 to-green-500/20
-        dark:from-yellow-500/10 dark:via-green-700/20 dark:to-green-800/20
+        bg-gradient-to-br
+        from-yellow-400/20
+        via-green-400/30
+        to-green-500/20
+        dark:from-yellow-500/10
+        dark:via-green-700/20
+        dark:to-green-800/20
         border border-gray-200 dark:border-white/10
         rounded-2xl p-8 shadow-2xl
         text-primary
