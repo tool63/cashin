6834c1d627bde19297d7443d0d1aae3bd6b672ee
tool63 <toolteller@gmail.com>
2026-03-05 22:02:42 +0600

    Update page.tsx

--- app/(site)/app-installs/page.tsx
+++ app/(site)/app-installs/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { buildSEO, SEOOutput } from "@/components/SEO/seoEngine";
