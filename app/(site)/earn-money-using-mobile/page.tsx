6db0131bc4ee1cb5fb5ae1a8cc4a35a3aac29cf4
tool63 <toolteller@gmail.com>
2026-03-05 17:59:16 +0600

    Update page.tsx

--- app/(site)/earn-money-using-mobile/page.tsx
+++ app/(site)/earn-money-using-mobile/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React, { useEffect, useState } from "react";
 import { ArrowRight, Zap, User, Gift, CheckCircle, ShieldCheck, ClipboardList, CreditCard } from "lucide-react";
 import Link from "next/link";
