9b44c2d11ca0246dbc8fa5ae8338857f4f93c313
tool63 <toolteller@gmail.com>
2026-03-05 18:02:50 +0600

    Update page.tsx

--- app/(site)/earn-xbox-gift-cards/page.tsx
+++ app/(site)/earn-xbox-gift-cards/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React, { useEffect, useState } from "react";
 import { ArrowRight, User, CreditCard, Gift, CheckCircle, ShieldCheck } from "lucide-react";
 import Link from "next/link";
