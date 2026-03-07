d6fd1533d545ff4cfce456759eed5b0fcaa6d1c2
tool63 <toolteller@gmail.com>
2026-03-08 00:48:56 +0600

    Update loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,12 @@
+// app/loading.tsx
+"use client";
+
 import GlobalLoading from "@/components/loading/GlobalLoading";
 
 export default function Loading() {
-  return <GlobalLoading />;
+  return (
+    <div className="fixed inset-0 z-50 bg-gray-100 dark:bg-[#070A14] flex items-center justify-center">
+      <GlobalLoading />
+    </div>
+  );
 }
