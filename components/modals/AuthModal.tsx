9727613f48ef1ce65183d7f43c20c13e3effbe4a
tool63 <toolteller@gmail.com>
2026-03-09 03:28:25 +0600

    Update AuthModal.tsx

--- components/modals/AuthModal.tsx
+++ components/modals/AuthModal.tsx
@@ -4,9 +4,10 @@ import { ReactNode } from "react";
 
 interface AuthModalProps {
   children: ReactNode;
+  onClose?: () => void; // Add optional onClose prop
 }
 
-export default function AuthModal({ children }: AuthModalProps) {
+export default function AuthModal({ children, onClose }: AuthModalProps) {
   return (
     <div className="relative">
       {children}
