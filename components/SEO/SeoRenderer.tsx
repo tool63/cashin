be9028c1fb2b11a2159aaa3fa8baa3649c25ef20
tool63 <toolteller@gmail.com>
2026-03-13 00:12:00 +0600

    Update SeoRenderer.tsx

--- components/SEO/SeoRenderer.tsx
+++ components/SEO/SeoRenderer.tsx
@@ -10,13 +10,18 @@ interface SeoRendererProps {
 export default function SeoRenderer({ seo }: SeoRendererProps) {
   const { metadata, canonical, preconnect, dnsPrefetch, preload } = seo;
 
+  // Fallbacks for SSR
+  const ogImages = metadata.openGraph?.images || [];
+  const twitterImages = metadata.twitter?.images || [];
+
   return (
     <Head>
-      <title>{metadata.title}</title>
+      <title>{metadata.title || "Cashog"}</title>
+
       {metadata.description && (
         <meta name="description" content={metadata.description} />
       )}
-      {metadata.keywords && (
+      {metadata.keywords?.length > 0 && (
         <meta name="keywords" content={metadata.keywords.join(", ")} />
       )}
       {metadata.robots && <meta name="robots" content={metadata.robots} />}
@@ -25,24 +30,43 @@ export default function SeoRenderer({ seo }: SeoRendererProps) {
         Object.entries(metadata.other).map(([key, value]) => (
           <meta key={key} name={key} content={value} />
         ))}
+
       {canonical && <link rel="canonical" href={canonical} />}
-      {preconnect &&
-        preconnect.map((url) => <link key={url} rel="preconnect" href={url} />)}
-      {dnsPrefetch &&
-        dnsPrefetch.map((url) => <link key={url} rel="dns-prefetch" href={url} />)}
-      {preload &&
-        preload.map((url) => <link key={url} rel="preload" href={url} />)}
+
+      {preconnect?.map((url) => (
+        <link key={url} rel="preconnect" href={url} />
+      ))}
+      {dnsPrefetch?.map((url) => (
+        <link key={url} rel="dns-prefetch" href={url} />
+      ))}
+      {preload?.map((url) => (
+        <link key={url} rel="preload" href={url} />
+      ))}
 
       {/* OpenGraph */}
       {metadata.openGraph && (
         <>
-          <meta property="og:title" content={metadata.openGraph.title} />
-          <meta property="og:description" content={metadata.openGraph.description} />
-          <meta property="og:type" content={metadata.openGraph.type} />
-          <meta property="og:url" content={metadata.openGraph.url} />
-          <meta property="og:site_name" content={metadata.openGraph.siteName} />
-          {metadata.openGraph.images?.map((img, i) => (
-            <meta key={i} property="og:image" content={img.url} />
+          {metadata.openGraph.title && (
+            <meta property="og:title" content={metadata.openGraph.title} />
+          )}
+          {metadata.openGraph.description && (
+            <meta property="og:description" content={metadata.openGraph.description} />
+          )}
+          {metadata.openGraph.type && (
+            <meta property="og:type" content={metadata.openGraph.type} />
+          )}
+          {metadata.openGraph.url && (
+            <meta property="og:url" content={metadata.openGraph.url} />
+          )}
+          {metadata.openGraph.siteName && (
+            <meta property="og:site_name" content={metadata.openGraph.siteName} />
+          )}
+          {ogImages.map((img, i) => (
+            <meta
+              key={i}
+              property="og:image"
+              content={img?.url || "/images/default-og.png"}
+            />
           ))}
         </>
       )}
@@ -50,10 +74,18 @@ export default function SeoRenderer({ seo }: SeoRendererProps) {
       {/* Twitter */}
       {metadata.twitter && (
         <>
-          <meta name="twitter:card" content={metadata.twitter.card} />
-          <meta name="twitter:site" content={metadata.twitter.site} />
-          {metadata.twitter.images?.map((img, i) => (
-            <meta key={i} name="twitter:image" content={img} />
+          {metadata.twitter.card && (
+            <meta name="twitter:card" content={metadata.twitter.card} />
+          )}
+          {metadata.twitter.site && (
+            <meta name="twitter:site" content={metadata.twitter.site} />
+          )}
+          {twitterImages.map((img, i) => (
+            <meta
+              key={i}
+              name="twitter:image"
+              content={img || "/images/default-og.png"}
+            />
           ))}
         </>
       )}
