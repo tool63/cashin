e398f220b16bb24a772010fb177b14c53a758eb4
tool63 <toolteller@gmail.com>
2026-03-13 01:27:51 +0600

    Update Skeleton.tsx

--- components/loading/Skeleton.tsx
+++ components/loading/Skeleton.tsx
@@ -9,7 +9,7 @@ export default function Skeleton({ className = "" }: SkeletonProps) {
     <div
       className={`relative overflow-hidden rounded-md bg-gray-200 dark:bg-white/10 ${className}`}
     >
-      <div className="absolute inset-0 -translate-x-full animate-[shimmer_1.6s_infinite] bg-gradient-to-r from-transparent via-white/40 to-transparent dark:via-white/10" />
+      <div className="absolute inset-0 -translate-x-full animate-shimmer bg-gradient-to-r from-transparent via-white/40 to-transparent dark:via-white/10" />
     </div>
   );
 }
