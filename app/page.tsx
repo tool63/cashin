e0d67dce9f6f8bed11d8743397f20f6ac8a2c5ab
tool63 <toolteller@gmail.com>
2026-03-12 21:29:11 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 import { redirect } from "next/navigation";
-import { LanguageDetector, defaultLanguage } from "./[lang]/core/detector";
 
 export default function RootPage() {
-  const detector = new LanguageDetector();
-  const lang = detector.detect() || defaultLanguage;
-
-  detector.setCookie(lang);
-
-  redirect(`/${lang}`);
+  redirect("/en");
 }
