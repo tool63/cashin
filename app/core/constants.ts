01bdd922eb6dca8cc97b2e488d035e3798c5df13
tool63 <toolteller@gmail.com>
2026-03-21 17:45:29 +0600

    Update constants.ts

--- app/core/constants.ts
+++ app/core/constants.ts
@@ -3,6 +3,11 @@ export const DEFAULT_LANGUAGE = "en";
 
 export const SUPPORTED_LANGUAGES = ["en", "fr", "de", "es", "pt"] as const;
 
+export type SupportedLanguage = (typeof SUPPORTED_LANGUAGES)[number];
+
+// ===============================
+// 🍪 COOKIE KEYS
+// ===============================
 export const COOKIE_KEYS = {
   COUNTRY: "USER_COUNTRY",
   LANGUAGE: "NEXT_LOCALE",
