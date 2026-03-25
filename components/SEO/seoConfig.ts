41310b6c494c60e31de78137cf8f1a8a9b8baf87
tool63 <toolteller@gmail.com>
2026-03-25 11:21:45 +0600

    Create seoConfig.ts

---
+++ components/SEO/seoConfig.ts
@@ -0,0 +1,25 @@
+export const SEO_CONFIG = {
+  siteName: "Cashog",
+  domain: "https://cashog.com",
+  defaultLanguage: "en",
+
+  titleTemplate: "%s | Cashog",
+
+  defaultTitle:
+    "Cashog - Earn Money Online with Surveys, Games & Offers",
+
+  defaultDescription:
+    "Earn real money online with Cashog. Complete surveys, play games, and unlock rewards worldwide.",
+
+  defaultKeywords: [
+    "earn money online",
+    "get paid online",
+    "make money apps",
+    "survey rewards",
+    "cash rewards",
+  ],
+
+  social: {
+    twitter: "@cashog",
+  },
+};
