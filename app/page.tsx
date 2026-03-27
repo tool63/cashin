b52b16a0e96b8a6274c88138d194810f94df863f
tool63 <toolteller@gmail.com>
2026-03-27 23:10:39 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,13 @@
-// app/page.tsx
 import { cookies } from "next/headers";
-import { DEFAULT_LANGUAGE, COOKIE_KEYS } from "@/app/core/constants";
-import { loadAllTranslations } from "@/app/core/i18n/loader";
-import { CountryProvider } from "@/app/[country]/providers/CountryProvider";
-import { LanguageProvider } from "@/app/[country]/providers/LanguageProvider";
-import CountryHomePage from "@/app/[country]/page";
+import { redirect } from "next/navigation";
+import { COOKIE_KEYS } from "@/app/core/constants";
 
-export default async function RootPage() {
+export default function RootPage() {
   const cookieStore = cookies();
-  
-  // Get saved country or default to US
-  const savedCountry = cookieStore.get(COOKIE_KEYS.COUNTRY)?.value || "us";
-  const language = cookieStore.get(COOKIE_KEYS.LANGUAGE)?.value as any || DEFAULT_LANGUAGE;
-  const isOverridden = !!cookieStore.get(COOKIE_KEYS.USER_LANGUAGE_OVERRIDE)?.value;
-  
-  // Load translations
-  const translations = await loadAllTranslations(language);
 
-  return (
-    <CountryProvider initialCountry={savedCountry}>
-      <LanguageProvider
-        initialLanguage={language}
-        translations={translations}
-        isOverridden={isOverridden}
-      >
-        <CountryHomePage />
-      </LanguageProvider>
-    </CountryProvider>
-  );
+  const savedCountry =
+    cookieStore.get(COOKIE_KEYS.COUNTRY)?.value || "us";
+
+  // Redirect to country route
+  redirect(`/${savedCountry}`);
 }
