4e4b655f135c3f3d1104f60e2bffea1aaedff0c7
tool63 <toolteller@gmail.com>
2026-03-27 23:13:27 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function RootPage() {
   const cookieStore = cookies();
 
   const savedCountry =
-    cookieStore.get(COOKIE_KEYS.COUNTRY)?.value || "us";
+    cookieStore.get(COOKIE_KEYS.COUNTRY)?.value || "/";
 
   // Redirect to country route
   redirect(`/${savedCountry}`);
