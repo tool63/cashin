aed7df9585c3aed69f52a9e099ac1a6a3ec3b159
tool63 <toolteller@gmail.com>
2026-04-01 20:13:10 +0600

    Update page.tsx

--- app/[country]/page.tsx
+++ app/[country]/page.tsx
@@ -136,7 +136,6 @@ export default async function HomePage({
           <HeroSection
             data={heroData}
             translations={typing}
-            countryName={countryName}
           />
         </CircleBorder>
       </div>
