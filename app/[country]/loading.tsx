9ce13decf8231fc1af40a50fa59f057d9126a9cd
tool63 <toolteller@gmail.com>
2026-04-10 14:56:37 +0600

    Fix loading component structure and remove padding

--- app/[country]/loading.tsx
+++ app/[country]/loading.tsx
@@ -5,7 +5,7 @@ import TableSkeleton from "@/components/loading/TableSkeleton";
 
 export default function Loading() {
   return (
-    <div className="min-h-screen w-full bg-gray-100 dark:bg-[#070A14] p-6">
+    <div className="min-h-screen w-full bg-gray-100 dark:bg-[#070A14]">
       <TableSkeleton rows={10} />
     </div>
   );
