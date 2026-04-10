333a97759c8c9c718188fc10f163a83dced9c273
tool63 <toolteller@gmail.com>
2026-04-10 14:53:44 +0600

    Update loading.tsx

--- app/[country]/loading.tsx
+++ app/[country]/loading.tsx
@@ -5,10 +5,8 @@ import TableSkeleton from "@/components/loading/TableSkeleton";
 
 export default function Loading() {
   return (
-    <div className="fixed inset-0 z-50 bg-gray-100 dark:bg-[#070A14] flex items-center justify-center p-6">
-      <div className="w-full max-w-6xl mx-auto">
-        <TableSkeleton rows={10} />
-      </div>
+    <div className="min-h-screen w-full bg-gray-100 dark:bg-[#070A14] p-6">
+      <TableSkeleton rows={10} />
     </div>
   );
 }
