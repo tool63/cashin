cc63138dd205adbabaa18ad54962edd019667872
tool63 <toolteller@gmail.com>
2026-04-10 14:50:44 +0600

    Replace GlobalLoading with TableSkeleton in loading.tsx

--- app/[country]/loading.tsx
+++ app/[country]/loading.tsx
@@ -1,12 +1,14 @@
 // app/loading.tsx
 "use client";
 
-import GlobalLoading from "@/components/loading/GlobalLoading";
+import TableSkeleton from "@/components/loading/TableSkeleton";
 
 export default function Loading() {
   return (
-    <div className="fixed inset-0 z-50 bg-gray-100 dark:bg-[#070A14] flex items-center justify-center">
-      <GlobalLoading />
+    <div className="fixed inset-0 z-50 bg-gray-100 dark:bg-[#070A14] flex items-center justify-center p-6">
+      <div className="w-full max-w-6xl mx-auto">
+        <TableSkeleton rows={10} />
+      </div>
     </div>
   );
 }
